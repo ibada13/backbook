5073141d9168a30c8fd13aaa77e0975396655859
hunter666hz <hzhunter7@gmail.com>
2025-03-05 23:40:52 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -20,27 +20,31 @@ Route::middleware(['auth:sanctum' , 'king'])->group(function(){
     Route::get('/mods' , [UserController::class , 'Get_Mods']);
     Route::put('/{id}/mod',[UserController::class , 'ModUser']);
     Route::put('/{id}/admin',[UserController::class , 'AdminUser']);
+    Route::get('/books/pending' , [BookController::class , 'Get_PENDING_Books'])->middleware('citizen');
 
 
 });
+Route::middleware(['auth:sanctum' ])->group(function(){
+    Route::post('/book' , [BookController::class , 'postbook']);
+    Route::post('/book/start-reading/{id}' , [BookController::class , 'startReadingBook']);
+    Route::put('/book/{id}/update-pages' , [BookController::class , 'updatePagesRead']);
+    Route::post('/book/{id}/favorite' , [BookController::class , 'favoriteit']);
+    Route::post('/book/{id}/save' , [BookController::class , 'saveit']);
+    
+    Route::get('/books/read' , [BookController::class , 'Get_Reading_Books']);
+    Route::get('/books/readed' , [BookController::class , 'Get_Readed_Books']);
+    Route::get('/books/saved' , [BookController::class , 'Get_Saved_Books']);
+    Route::get('/books/favorite' , [BookController::class , 'Get_Favorited_Books']);
+    Route::get('/books/published' , [BookController::class , 'Get_Published_Books']);
+    Route::post('/comments' , [CommentController::class , 'postcomment']);
+    Route::put('/comments/{id}' , [CommentController::class , 'update']);
+    Route::delete('/comments/{id}' , [CommentController::class , 'deletecomment']);
+});
+
 Route::get('/books' , [BookController::class , 'getBooks']);
 Route::get('/book' , [BookController::class , 'getBook']);
-Route::post('/book' , [BookController::class , 'postbook']);
-Route::post('/book/start-reading/{id}' , [BookController::class , 'startReadingBook']);
-Route::put('/book/{id}/update-pages' , [BookController::class , 'updatePagesRead']);
-Route::post('/book/{id}/favorite' , [BookController::class , 'favoriteit'])->middleware('auth:sanctum');
-Route::post('/book/{id}/save' , [BookController::class , 'saveit'])->middleware('auth:sanctum');
-Route::get('/books/pending' , [BookController::class , 'Get_PENDING_Books'])->middleware('citizen');
-Route::get('/books/read' , [BookController::class , 'Get_Reading_Books'])->middleware('auth:sanctum');
-Route::get('/books/readed' , [BookController::class , 'Get_Readed_Books'])->middleware('auth:sanctum');
-Route::get('/books/saved' , [BookController::class , 'Get_Saved_Books'])->middleware('auth:sanctum');
 Route::get('/books/popular' , [BookController::class , 'Get_Popular_Books']);
-Route::get('/books/favorite' , [BookController::class , 'Get_Favorited_Books'])->middleware('auth:sanctum');
-Route::get('/books/published' , [BookController::class , 'Get_Published_Books'])->middleware('auth:sanctum');
 // Route::put('/users/{id}/ghost');
 
 Route::get('/comments' , [CommentController::class , 'getcommentsforbook']);
-Route::post('/comments' , [CommentController::class , 'postcomment']);
-Route::delete('/comments/{id}' , [CommentController::class , 'deletecomment']);
-Route::put('/comments/{id}' , [CommentController::class , 'update']);
 
