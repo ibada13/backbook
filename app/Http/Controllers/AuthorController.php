5f23948fc913ac7f3ffd1a40942ef0349093d0a2
hunter666hz <hzhunter7@gmail.com>
2025-03-09 22:33:13 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/Http/Controllers/AuthorController.php
    	modified:   routes/api.php

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -11,4 +11,22 @@ class AuthorController extends Controller
         $author->author_pfp = $author->author_pfp ?asset("images/authors/{$author->author_pfp}"):null ; 
         return response()->json($author);
     }
+    public function update(Request $request , $id){
+        $request->validate([
+            'bio' => 'required|string|max:500',
+        ]);
+    
+        $author = Author::find($id);
+    
+        if (!$author) {
+            return response()->json(['error' => 'author not found'], 404);
+        }
+    
+    
+    
+        $author->bio = $request->bio;
+        $author->save();
+    
+        return response()->json(['message' => 'author updated successfully'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware(['auth:sanctum','citizen'])->group(function () {
     Route::get('/users' , [UserController::class , 'Get_Users']);
     Route::delete('/mod/comments/{id}' , [CommentController::class , 'ModDeleteComment']);
     Route::put('/type/{id}' , [TypeController::class , 'update']);
+    Route::put('/author/{id}' , [AuthorController::class , 'update']);
 
 });
 Route::middleware(['auth:sanctum' , 'king'])->group(function(){
